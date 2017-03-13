06421dc187692532348c9edd1c16612b3ab3f9fd
Jasper Frumau <jasper@imagewize.com>
2017-03-13 17:44:54 +0300

    Create sync-db-from-dev.sh

---
+++ sync-db-from-dev.sh
@@ -0,0 +1,11 @@
+# sync-db-from-dev.sh
+read -r -p "Do you solemnly swear that you have had fewer than 2 alcoholic beverages in the last hour and that you would really like to reset your development database and pull the latest from production? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    wp @production db reset --yes
+    wp @development db export - > sql-dump-production.sql
+    wp @production db import sql-dump-production.sql
+    wp @production search-replace https://imagewize.dev https://imagewize.nl --skip-columns=guid
+else
+    exit 0
+fi
